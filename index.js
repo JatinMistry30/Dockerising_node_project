ce7a0f2943ec844c9e1c60df2c355a4ed2b56e95
jatin9975 <jatinmistry9975@gmail.com>
2024-12-21 13:09:29 +0530

    Git commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
-
+const dotenv = require('dotenv')
 const app = express()
+
+dotenv.config();
+
+
 
 app.get('/home',(req,res)=>{
     res.json({message:"ok"})
 })
 
-app.listen(3000,() =>{
+app.listen(process.env.PORT,() =>{
     console.log("Server is running")
 })
